6f4f71e37913ec9e6dca6a93d1d4ab58776581f8
Stephen Russell <stephenjohnrussell@me.com>
2017-09-18 15:27:18 +0100

    little main cleaning

--- spectrometer.go
+++ spectrometer.go
@@ -7,56 +7,45 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"time"
 )
 
 func main() {
 	fmt.Println("Raspberry PI Spectrometer")
-	fileName := time.Now().Format("2006-01-02_15:04::05") + ".jpg"
-	cmd := exec.Command("raspistill", "-o", fileName)
-	_, err := cmd.StdoutPipe()
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = cmd.Start()
-	if err != nil {
-		fmt.Println(err)
-	}
-	cmd.Wait()
 
-	reader, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer reader.Close()
-	m, _, err := image.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bounds := m.Bounds()
-
-	// Calculate a 16-bin histogram for m's red, green, blue and alpha components.
-	//
-	// An image's bounds do not necessarily start at (0, 0), so the two loops start
-	// at bounds.Min.Y and bounds.Min.X. Looping over Y first and X second is more
-	// likely to result in better memory access patterns than X first and Y second.
-	var histogram [16][4]int
+	SaveImage()
+	img := OpenImage()
+	bounds := img.Bounds()
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			r, g, b, a := m.At(x, y).RGBA()
-			log.Println(r, g, b)
-			// A color's RGBA method returns values in the range [0, 65535].
-			// Shifting by 12 reduces this to the range [0, 15].
-			histogram[r>>12][0]++
-			histogram[g>>12][1]++
-			histogram[b>>12][2]++
-			histogram[a>>12][3]++
+			r, g, b, a := img.At(x, y).RGBA()
+			fmt.Printf("At(%v,%v) : R%v G%v B%v A%v", x, y, r, g, b, a)
 		}
 	}
+}
 
-	// Print the results.
-	fmt.Printf("%-14s %6s %6s %6s %6s\n", "bin", "red", "green", "blue", "alpha")
-	for i, x := range histogram {
-		fmt.Printf("0x%04x-0x%04x: %6d %6d %6d %6d\n", i<<12, (i+1)<<12-1, x[0], x[1], x[2], x[3])
+//SaveImage from camera
+func SaveImage() {
+	cmd := exec.Command("raspistill", "-w", "-o", "image.jpg")
+	_, err := cmd.StdoutPipe()
+	LogFatal(err)
+	err = cmd.Start()
+	LogFatal(err)
+	cmd.Wait()
+}
+
+//OpenImage and load it into memory
+func OpenImage() image.Image {
+	reader, err := os.Open("image.jpg")
+	LogFatal(err)
+	defer reader.Close()
+	img, _, err := image.Decode(reader)
+	LogFatal(err)
+	return img
+}
+
+//LogFatal quit program if we get an error
+func LogFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
